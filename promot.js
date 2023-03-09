080884ebd7a083a4e72ce7b53d2fdb689000febe
github_username <you@domain.com>
2023-03-09 23:50:03 +0600

    promot functional added

--- promot.js
+++ promot.js
@@ -1,1 +1,14 @@
-console.log('alert')
+// console.log('alert')
+const promotAlrt = () =>{
+    confirm('load data');
+}
+
+const promotClick = () =>{
+   const nama  = prompt('data load');
+   if(nama  === null){
+    alert('nam na dile kam nai')
+   }
+   else{
+    alert('tomar nam thik ace')
+   }
+}
